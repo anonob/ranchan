047e4b235555643dd1fcfbf80d3e4a393274577d
Anon <anonoblesseoblige@gmail.com>
2016-06-03 00:50:43 -0700

    add jqeury/ajax proxy functionality for 4chan api, has possible memory leak

--- public/scripts/thumbnails.js
+++ public/scripts/thumbnails.js
@@ -1,54 +1,96 @@
 $(function () {
 	var $container = $("#container"),
+		thumbId = 0,
+		thumbQ = [],
 		pageNum = 0,
 		pageCount = 0,
 		threadNum = 0,
 		threadCount = 0,
-		site = "4chan",
-		board = "a",
-		boardjson = "http://a.4cdn.org/a/catalog.json",
-		sites = ["4chan"];
+		chans = [{"site":"4chan", "boards":"http://a.4cdn.org/boards.json"}],
+		chan = null,
+		board = null,
+		catalog = null,
+		i = 0,
+		thread = null,
+		top = "",
+		left = "",
+		removeId = "";
 
-	site = sites[randInt(0, sites.length - 1)];
+	chan = chans[randInt(0, chans.length - 1)];
 
-	if(site == "4chan") {
-		$.ajax({
-			type: "GET",
-			url: "http://a.4cdn.org/boards.json",
-			data: {format: "json"},
-			dataType: "jsonp",
-			success: function(data) {
-				console.log(data);
-				boardjson = "http://a.4cdn.org/" + data.boards[randInt(0, data.boards.length - 1)].board + "/catalog.json";
-			},
+	genThumb();
+
+	function getBoard() {
+		return $.ajax({
+			type: "POST",
+			url: "http://localhost:5000/thumb",
+			data: JSON.stringify(chan),
+			contentType: "application/json",
+			dataType: "json",
 			error: function(data) {
+				console.log("An error occured while obtaining a board");
 				console.log(data);
-				console.log("err");
 			}
 		});
+	}
 
-		$.ajax({
-			type: "GET",
-			url: boardjson,
-			data: "",
-			success: function(feed) {
-				for(var i = 0; i < feed.length - 1; i++) {
-					pageCount++;
-				}
-				pageNum = randInt(0, pageCount);
-
-				for(var i = 0; i < feed[pageNum].threads.length - 1; i++) {
-					threadCount++;
-				}
-				threadNum = randInt(0, threadCount);
-				var thread = {"no":feed[pageNum].threads[threadNum].no, "tim":feed[pageNum].threads[threadNum].tim, "ext":feed[pageNum].threads[threadNum].ext};
-				
-				console.log(thread.no + "/" + thread.tim);
-
-				$container.append("<img class=\"thumb\" src=\"i.4cdn.org/" + thread.board + "/" + thread.tim.toString() + thread.ext + 
-					"\" href=\"boards.4chan.org/" + board + "/thread/" + thread.no.toString() + " alt=\"\>current year \>using alt\">");
-			},
-			dataType: "jsonp"
+	function getCatalog(boardData, textStatus, jqXHR) {
+		do {
+			board = boardData.boards[randInt(0, boardData.boards.length - 1)].board;
+		} while(board == "swf" || board == "adv")
+		
+		catalog = {"board":board, "threads":"http://a.4cdn.org/" + board + "/catalog.json"};
+		
+		return $.ajax({
+			type: "POST",
+			url: "http://localhost:5000/thumb",
+			data: JSON.stringify(catalog),
+			contentType: "application/json",
+			dataType: "json",
+			error: function(data) {
+				console.log("An error occured while obtaining a thread");
+				console.log(data);
+			}
 		});
+	}
+
+	function getThread(catalogData, textStatus, jqXHR) {
+		do {
+			for(i = 0; i < catalogData.length - 1; i++) {
+				pageCount++;
+			}
+			pageNum = randInt(0, pageCount);
+			pageCount = 0;
+
+			for(i = 0; i < catalogData[pageNum].threads.length - 1; i++) {
+				threadCount++;
+			}
+			threadNum = randInt(0, threadCount);
+			threadCount = 0;
+
+			thread = {"no":catalogData[pageNum].threads[threadNum].no.toString(), "tim":catalogData[pageNum].threads[threadNum].tim.toString(), "ext":catalogData[pageNum].threads[threadNum].ext};
+		} while(thread.ext == ".swf" || thread.ext == ".webm")
+		
+		
+		top = randInt(5, 75).toString() + "%";
+		left = randInt(5, 75).toString() + "%";
+		$container.append("<a class=\"thumb\" id=\"id-" + thumbId.toString() + "\" target=\"_blank\" href=\"http://boards.4chan.org/" 
+			+ board + "/thread/" + thread.no + "\"><img src=\"http://i.4cdn.org/" + board + "/" + thread.tim + thread.ext 
+			+ "\" style=\"top:" + top + ";left:" + left + "\" alt=\"\>current year \>using alt\"></a>");
+
+		thumbQ.push(thumbId);
+		thumbId++;
+		if(thumbId == 20) {
+			thumbId = 0;
+		}
+	}
+
+	function genThumb() {
+		getBoard().then(getCatalog).then(getThread);
+		if(thumbQ.length > 5) {
+			removeId = "#id-" + thumbQ.shift().toString();
+			$(removeId).remove();
+		}
+		setTimeout(genThumb, 3000);
 	}
 });
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+app.post('/thumb', function(req, res) {
+	if(req.body.boards != undefined) {
+		request(req.body.boards).pipe(res);
+		console.log("Board: " + req.body.boards);
+	} else if(req.body.threads != undefined) {
+		request(req.body.threads).pipe(res);
+		console.log("Thread: " + req.body.threads);
+	}
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('>ranchan is running on port', app.get('port'));
 });
