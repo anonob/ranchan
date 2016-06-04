966281881c2c1cf3417ee01c4a1f7d832daf2286
Anon <anonoblesseoblige@gmail.com>
2016-06-03 20:14:27 -0700

    switch urls to dns

--- public/scripts/thumbnails.js
+++ public/scripts/thumbnails.js
@@ -31,7 +31,7 @@ function thumbnails() {
 	function getBoard() {
 		return $.ajax({
 			type: "POST",
-			url: "http://localhost:5000/boards",
+			url: "http://ranchan.moe/boards",
 			data: JSON.stringify(chan),
 			contentType: "application/json",
 			dataType: "json",
@@ -51,7 +51,7 @@ function thumbnails() {
 		
 		return $.ajax({
 			type: "POST",
-			url: "http://localhost:5000/threads",
+			url: "http://ranchan.moe/threads",
 			data: JSON.stringify(catalog),
 			contentType: "application/json",
 			dataType: "json",
