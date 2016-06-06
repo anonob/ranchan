a37b661fd60e16e2f7985de34794960d6bac1b21
Anon <anonoblesseoblige@gmail.com>
2016-06-06 01:38:57 -0700

    ver:0.1.1 fix image missing/deleted issues, add readme, minor style improvements

--- public/scripts/thumbnails.js
+++ public/scripts/thumbnails.js
@@ -2,30 +2,26 @@ function thumbnails() {
 	var $container = $("#container"),
 		thumbId = 0,
 		thumbQ = [],
-		pageNum = 0,
-		pageCount = 0,
-		threadNum = 0,
-		threadCount = 0,
-		chans = [{"site":"4chan", "boards":"http://a.4cdn.org/boards.json"}],
-		i = 0,
-		top = "",
-		left = "",
-		removeId = "";
+		chans = [{"site":"4chan", "boards":"http://a.4cdn.org/boards.json"}];
 
-	setInterval(genThumbs, 300);
+	setInterval(genThumbs, 350);
 
 	function genThumbs() {
-		var randChan = chans[randInt(0, chans.length - 1)];
+		var randomChan = chans[randInt(0, chans.length - 1)];
 		var thread = {"tid":"", "chan":{"site":"", "boards":""}, "board":{"letter":"", "catalog":""}, "op":{"no":"", "tim":"", "ext":""}};
 		thread.tid = thumbId.toString();
-		thread.chan.site = randChan.site;
-		thread.chan.boards = randChan.boards;
+		thread.chan.site = randomChan.site;
+		thread.chan.boards = randomChan.boards;
 		thumbId++;
 
-		if(thumbQ.length > 8) {
-			removeId = "#tid-" + thumbQ.shift().toString();
+		if(thumbQ.length > 9) {
+			var removeId = "#tid-" + thumbQ.shift().toString();
 			$(removeId).remove();
 		}
+		if(thumbId >= 32) {
+			thumbId = 0;
+		}
+
 		getBoard(thread);
 	}
 
@@ -74,33 +70,42 @@ function thumbnails() {
 	}
 
 	function getThread(thread, catalogData) {
+		var filetype = "",
+			top = "",
+			left = "",
+			pageNum = 0,
+			threadNum = 0
+			imgsrc = "";
+
 		do {
-			for(i = 0; i < catalogData.length - 1; i++) {
-				pageCount++;
-			}
-			pageNum = randInt(0, pageCount);
-			pageCount = 0;
+			pageNum = randInt(0, catalogData.length - 1);
+			threadNum = randInt(0, catalogData[pageNum].threads.length - 1);
 
-			for(i = 0; i < catalogData[pageNum].threads.length - 1; i++) {
-				threadCount++;
+			thread.op.no = catalogData[pageNum].threads[threadNum].no.toString();
+			if(catalogData[pageNum].threads[threadNum].tim == undefined) {
+				if(catalogData[pageNum].threads[threadNum].filedeleted == 1) {
+					imgsrc = "http://s.4cdn.org/image/filedeleted.gif";
+				} else {
+					imgsrc = "http://s.4cdn.org/image/nofile.png";
+				}
+			} else {
+				thread.op.tim = catalogData[pageNum].threads[threadNum].tim.toString();
+				thread.op.ext = catalogData[pageNum].threads[threadNum].ext;
+				if(thread.op.ext == ".gif") {
+					filetype = ".gif";
+				} else {
+					filetype = "s.jpg";
+				}
+				imgsrc = "http://i.4cdn.org/" + thread.board.letter + "/" + thread.op.tim + filetype;
 			}
-			threadNum = randInt(0, threadCount);
-			threadCount = 0;
-
-			thread.op.no = catalogData[pageNum].threads[threadNum].no.toString()
-			thread.op.tim = catalogData[pageNum].threads[threadNum].tim.toString()
-			thread.op.ext = catalogData[pageNum].threads[threadNum].ext;
 		} while(thread.op.ext == ".swf" || thread.op.ext == ".webm" || thread.op.ext == ".pdf")
-		
+
 		top = randInt(5, 70).toString() + "%";
-		left = randInt(5, 80).toString() + "%";
+		left = randInt(5, 85).toString() + "%";
 		$container.append("<a class=\"thumb\" id=\"tid-" + thread.tid + "\" target=\"_blank\" href=\"http://boards.4chan.org/" 
-			+ thread.board.letter + "/thread/" + thread.op.no + "\"><img src=\"http://i.4cdn.org/" + thread.board.letter + "/" 
-			+ thread.op.tim + thread.op.ext + "\" style=\"top:" + top + ";left:" + left + "\" alt=\"\>current year \>using alt\"></a>");
+			+ thread.board.letter + "/thread/" + thread.op.no + "\"><img src=\"" + imgsrc + "\" style=\"top:" + top + ";left:" 
+			+ left + "\" alt=\"\>current year \>seeing alt-text\"></a>");
 
 		thumbQ.push(parseInt(thread.tid, 10));
-		if(thumbId == 32) {
-			thumbId = 0;
-		}
 	}
 }
